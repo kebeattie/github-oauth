c07b301af426786f5cfd261590c0afaeaa7caf24
kebeattie <kebeattie98@hotmail.com>
2024-06-03 11:07:31 +0100

    OAuth for Github implemented

--- app.js
+++ app.js
@@ -6,9 +6,10 @@ const path = require("path");
 require("dotenv").config();
 const express = require('express');
 const partials = require('express-partials');
-
-
+const session = require('express-session');
 const app = express();
+const passport = require('passport');
+const GitHubStrategy = require('passport-github2').Strategy;
 
 
 /*
@@ -22,8 +23,21 @@ const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
 /*
  * Passport Configurations
 */
+passport.use(new GitHubStrategy({
+  clientID: GITHUB_CLIENT_ID,
+  clientSecret: GITHUB_CLIENT_SECRET,
+  callbackURL: 'http://localhost:3000/auth/github/callback'
+}, function(accessToken, refreshToken, profile, done) {
+      return (done(null, profile));
+}));
 
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
 
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
 
 
 
@@ -38,19 +52,28 @@ app.set('view engine', 'ejs');
 app.use(partials());
 app.use(express.json());
 app.use(express.static(__dirname + '/public'));
-
-
+app.use(session({
+  secret: 'codecademy',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 /*
  * Routes
 */
+const ensureAuthenticated = (req, res, next) => {
+  if(req.isAuthenticated()){ return next() };
+  res.redirect('/redirect'); 
+}
 
 app.get('/', (req, res) => {
   res.render('index', { user: req.user });
 })
 
-app.get('/account', (req, res) => {
+app.get('/account', ensureAuthenticated,(req, res) => {
   res.render('account', { user: req.user });
 });
 
@@ -63,7 +86,16 @@ app.get('/logout', (req, res) => {
   res.redirect('/');
 });
 
+app.get('/auth/github',
+  passport.authenticate('github', {scope: ['user']}));
 
+app.get('/auth/github/callback',
+  passport.authenticate('github', {
+    failureRedirect: '/login',
+    successRedirect: '/'
+  })
+  
+)
 
 
 /*
